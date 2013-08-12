ee46ba1162bc18c302120281711fd7a0ea51cf5c
Agustin Feuerhake <agustin@platan.us>
2013-08-12 17:31:24 -0400

    improves factories

--- spec/factories/rates.rb
+++ spec/factories/rates.rb
@@ -1,12 +1,22 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :rate do |r|
-    r.direction {["forward","back"].sample}
-    r.rate_type {[:SM,:AD].sample}
-    r.rate_desc {{SM:'Smiles',AD:'Smiles Any Day'}[self.rate_type]}
-    r.rate {[10,11,12,13,14,15].sample * 1000}
-    r.flight {"G3-" + (rand(290) + 1000).to_s}
-    r.trip
+  factory :rate do
+    trait :tam do
+      rate_type {[:T0,:T1,:T2,:T3].sample}
+      rate_desc {{T0:'Promo',T1:'Economic',T2:'Regular',T3:'Flexible'}[self.rate_type]}
+      flight {"JJ-" + (rand(790) + 3000).to_s}
+      rate {[18,19,20,22,24,26].sample * 1000}
+      association :trip, :tam
+    end
+    trait :gol do
+      rate_type {[:SM,:AD].sample}
+      rate_desc {{SS:'Smile',AD:'Smile Any Day',SM:'Smile And Money',SR:'Smile Reduzida'}[self.rate_type]}
+      flight {"G3-" + (rand(790) + 3000).to_s}
+      rate {[10,11,12,13,14,15].sample * 1000}
+      association :trip, :gol
+    end
+    direction {["forward","back"].sample}
+   
   end
 end
--- spec/factories/trips.rb
+++ spec/factories/trips.rb
@@ -2,11 +2,16 @@
 
 FactoryGirl.define do
 	d = Date.today
-  factory :trip do |t|
-    t.operator "GOL"
-    t.departing {d + rand(90)}
-    t.returning {self.departing + rand(45)}
-    t.origin {["SAO", "RIO", "BHZ", "BSB"].sample}
-    t.destination {["FOR", "REC", "SSA", "NAT"].sample}
+  factory :trip do
+  	trait :tam do
+    	operator "TAM"
+    end
+    trait :gol do
+    	operator "GOL"
+    end
+    departing {d + rand(90)}
+    returning {self.departing + rand(45)}
+    origin {["SAO", "RIO", "BHZ", "BSB"].sample}
+    destination {["FOR", "REC", "SSA", "NAT"].sample}
   end
 end
